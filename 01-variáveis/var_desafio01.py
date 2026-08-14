1653d731aa762018d00b0b15eb337633a60da883
DouglasM10112010 <douglas.filho10112010@gmail.com>
2026-08-13 21:27:53 -0300

    Variáveis interas

---
+++ 01-variáveis/var_desafio01.py
@@ -0,0 +1,8 @@
+# Autor: Douglas Magalhães
+# desafio do curso fundamentos python " MORE BEM "
+
+largura = 40
+comprimento = 50
+area = largura * comprimento
+print(area)
+print("A área é:", area)
---
+++ 01-variáveis/var_int.py
@@ -0,0 +1,16 @@
+# Autor: Douglas Magalhães
+# estudando variáves inteiras
+
+# Nome e tipo
+valor1 = 12
+valor2 = 22
+soma = valor1 + valor2
+subitracao = valor1 - valor2
+mutiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+print(soma)
+print(subitracao)
+print(mutiplicacao)
+print(divisao)
+print(soma)
+
