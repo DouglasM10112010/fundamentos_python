977a88f78485b1db1b263d50e886f59a9daccb7a
DouglasM10112010 <douglas.filho10112010@gmail.com>
2026-08-20 18:46:47 -0300

    Add files via upload

---
+++ 01-variáveis/var_desafio01.py
@@ -0,0 +1,17 @@
+# Autor: Douglas Magalhães
+# desafio do curso fundamentos python " MORE BEM "
+
+largura = 40
+comprimento = 50
+area = largura * comprimento
+print(area)
+print("A área é:", Area)
+ 
+ # desafio 2
+ largura = 40
+comprimento = 50
+Valor = 45
+area = largura * comprimento
+valor = area * Valor
+print(area)
+print("O valor da área custa:", valor)
