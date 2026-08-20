811f26bca07bbf5d7060ac66aa6703d463e028b3
DouglasM10112010 <douglas.filho10112010@gmail.com>
2026-08-19 21:41:15 -0300

    Add files via upload

---
+++ 01-variáveis/var_float.py
@@ -0,0 +1,15 @@
+#Autor: Douglas Magalhães
+#Projeto: VAriáveis decimais
+
+idade = 15 #variável inteira
+altura = 1,78 #variável decimal
+peso = 71,3 #variável decimal
+nome = 'Douglas' #variável nome
+
+print(f'meu nome é {nome}, minha idade é {idade}, minha altura é {altura}, meu peso é {peso}')
+
+
+
+
+
+
