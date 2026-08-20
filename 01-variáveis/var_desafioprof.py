a025d09743074c43871b6eb598684815908568af
DouglasM10112010 <douglas.filho10112010@gmail.com>
2026-08-19 21:11:49 -0300

    Add files via upload

---
+++ 01-variáveis/var_desafioprof.py
@@ -0,0 +1,10 @@
+# Autor: Douglas Magalhães
+# Projeto:  Sistema de conversção de dinheiro (dolar em reais)
+
+dolar = float(input('Qual é o valor'))
+
+Real = dolar * 5.18
+
+resultado = Real
+
+print(f'o valor, em reais, é {resultado}')
