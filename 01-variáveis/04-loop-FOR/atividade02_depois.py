a162d5e77cb3d3c92625496bff393861d7724fc4
DouglasM10112010 <douglas.filho10112010@gmail.com>
2026-08-26 19:53:38 -0300

    Delete 01-variáveis/04-loop-FOR directory

--- 01-variáveis/04-loop-FOR/atividade02_depois.py
+++
@@ -1,9 +0,0 @@
-#Autor: Douglas Magalhães
-#Projeto: Aprendendo Loop FOR
-
-#variavel = i
-tabuada = 2
-
-for i in range (1,11):
-    #print(f'{tabuada} x 1 = {1*tabuada}')
-    print(f'{tabuada} x {i} = {tabuada*i}')
