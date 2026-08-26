1361a2aa94244bdc169045772e7933ce4293aa78
DouglasM10112010 <douglas.filho10112010@gmail.com>
2026-08-26 19:54:16 -0300

    Add files via upload

---
+++ 04-loop-FOR/atividade01_for_depois.py
@@ -0,0 +1,20 @@
+#Autor: Douglas Magalhães
+#Projeto: Aprendendo Loop FOR
+
+#Loop FOR precisa de uma variável
+#variavel = i
+
+for i in range (1,11):
+    print(i)
+
+#i = 1
+#i = i + 1 = 1 + 1 = 2
+#i = i + 1 = 2 + 1 = 3
+#i = i + 1 = 3 + 1 = 4
+#i = i + 1 = 4 + 1 = 5
+#i = i + 1 = 5 + 1 = 6
+#i = i + 1 = 6 + 1 = 7
+#i = i + 1 = 7 + 1 = 8
+#i = i + 1 = 8 + 1 = 9
+#i = i + 1 = 9 + 1 = 10
+#i = i + 1 = 10 + 1 = 11 - sai do loop
