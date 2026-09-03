f6390f9a23b31f8e07a305dd8e2f581a5b5d1819
DouglasM10112010 <douglas.filho10112010@gmail.com>
2026-09-03 19:38:22 -0300

    Add files via upload

---
+++ 09-funcoes/atividade02_fucoes.py
@@ -0,0 +1,38 @@
+#Autor: Douglas Magalhaes
+#Projeto: trabalhando com fuções
+
+'''
+valor1 = float(input('Digite o primeiro valor: '))
+valor2 = float(input('digite o segundo valor: '))
+soma = valor1 + valor2
+subitracao = valor1 - valor2
+mutiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+print(f'O valor da soma é: {soma:.2f}')
+print(f'O valor da subitração é: {subitracao:.2f}')
+print(f'O valor da multiplicação é: {mutiplicacao:.2f}')
+print(f'O valor da divisão é: {divisao:.2f}')
+'''
+
+def calc_basica(v1, v2):
+    soma = v1 + v2
+    subitracao = v1 - v2
+    mutiplicacao = v1 * v2
+    divisao = v1 / v2
+    return soma
+    return subitracao
+    return mutiplicacao
+    return divisao
+
+valor1 = float(input('Digite o primeiro valor: '))
+valor2 = float(input('digite o segundo valor: '))
+soma = valor1 + valor2
+subitracao = valor1 - valor2
+mutiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+print(f'O valor da soma é: {soma:.2f}')
+print(f'O valor da subitração é: {subitracao:.2f}')
+print(f'O valor da multiplicação é: {mutiplicacao:.2f}')
+print(f'O valor da divisão é: {divisao:.2f}')
+
+
